c0fcebc2ed98ab9c52379564cf21a6c2bbb45a69
Hana Mengistu <hmengistu3@student.cccs.edu>
2021-11-23 16:06:57 -0700

    Add Gender column

--- Hospital Database Project/dbo/Tables/Patient.sql
+++ Hospital Database Project/dbo/Tables/Patient.sql
@@ -2,6 +2,7 @@
     [PatientID] INT          IDENTITY (1, 1) NOT NULL,
     [FirstName] VARCHAR (50) NOT NULL,
     [LastName]  VARCHAR (50) NULL,
+    [Geneder] CHAR(1) NULL, 
     CONSTRAINT [PK_Patient_PatientID] PRIMARY KEY CLUSTERED ([PatientID] ASC)
 );
 
