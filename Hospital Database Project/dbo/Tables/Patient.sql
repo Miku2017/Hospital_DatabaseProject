5444416994cabcc2867943ff20e622898d45cbcf
Hana Mengistu <hmengistu3@student.cccs.edu>
2021-11-23 12:37:56 -0700

    Add FirstName column

--- Hospital Database Project/dbo/Tables/Patient.sql
+++ Hospital Database Project/dbo/Tables/Patient.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Patient] (
     [PatientID] INT IDENTITY (1, 1) NOT NULL,
+    [FirstName]  Varchar(50) NOT NULL,
     CONSTRAINT [PK_Patient_PatientID] PRIMARY KEY CLUSTERED ([PatientID] ASC)
 );
 
