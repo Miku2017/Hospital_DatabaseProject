a5d464f357453642864e28deba7ebc731201eb54
Bisrat Gezahgne <68933864+Miku2017@users.noreply.github.com>
2021-11-23 12:39:44 -0700

    Update Patient.sql
    Add LastName

--- Hospital Database Project/dbo/Tables/Patient.sql
+++ Hospital Database Project/dbo/Tables/Patient.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Patient] (
     [PatientID] INT IDENTITY (1, 1) NOT NULL,
     [FirstName]  Varchar(50) NOT NULL,
+    [LastName]  Varchar(50) NOT NULL,    
     CONSTRAINT [PK_Patient_PatientID] PRIMARY KEY CLUSTERED ([PatientID] ASC)
 );
 
