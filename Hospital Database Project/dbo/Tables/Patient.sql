a7d8cf7d80a9e15329880f09397a3713043d7928
Hana Mengistu <hmengistu3@student.cccs.edu>
2021-11-23 16:09:39 -0700

    Add PhoneColumn

--- Hospital Database Project/dbo/Tables/Patient.sql
+++ Hospital Database Project/dbo/Tables/Patient.sql
@@ -3,6 +3,7 @@
     [FirstName] VARCHAR (50) NOT NULL,
     [LastName]  VARCHAR (50) NULL,
     [Geneder] CHAR(1) NULL, 
+    [Phone] VARCHAR(50) NULL, 
     CONSTRAINT [PK_Patient_PatientID] PRIMARY KEY CLUSTERED ([PatientID] ASC)
 );
 
